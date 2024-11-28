8c764d8cf4bc25b4ba9d8b9a63ffb3c01aa7562b
samsandeepedwin <66881212+samsandeepedwin@users.noreply.github.com>
2024-11-28 09:36:09 +0530

    Update 459_Repeated_Substring_Pattern.cs

--- 459_Repeated_Substring_Pattern.cs
+++ 459_Repeated_Substring_Pattern.cs
@@ -1,4 +1,5 @@
 //link: https://leetcode.com/problems/repeated-substring-pattern/description
+//my solution: https://leetcode.com/problems/repeated-substring-pattern/solutions/6090519/simple-c-solution-break-the-string-into-smaller-parts-repeat-the-pattern-and-check
 
 public class Solution {
     public bool RepeatedSubstringPattern(string s) {
@@ -22,6 +23,12 @@ public class Solution {
 }
 
 /*
+Approach: 
+Create substring from input string with length where input.Length % substring.Length == 0.
+Create new string using String.Builder.Append by repeating the substring from above to reach the input string length.
+Compare new string and input string.
+If same, then return true, else false.
+
 Example 1:
 Input "abcabcabcabc"
 Output :
